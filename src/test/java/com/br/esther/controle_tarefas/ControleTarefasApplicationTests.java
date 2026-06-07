a45da91b5932978dd5ff487b3d1f46e6a5081ff3
esther santos <esthermendonca13@gmail.com>
2026-06-07 12:54:04 -0300

    adiciona arquivos de configuracao do projeto

---
+++ src/test/java/com/br/esther/controle_tarefas/ControleTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.esther.controle_tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
